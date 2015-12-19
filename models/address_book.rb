369b3240e2f640e11f7617933270bf85c13755f4
Derek <hello@derekmartinez.net>
2015-12-19 16:05:47 -0500

    Assignment 22: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,7 +21,7 @@ def add_entry(name, phone_number, email)
 
 def import_from_csv(file_name)
   csv_text = File.read(file_name)
-  csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  csv = CSV.parse(csv_text, headers: true, skip_blanks: true )
 
   csv.each do |row|
     row_hash = row.to_hash
@@ -46,6 +46,15 @@ def binary_search(name)
   end
 end
 
+  return nil
+end
+
+def iterative_search(name)
+  @entries.each do |entry|
+    if entry.name == name
+      return entry
+    end
+  end
   return nil
 end
 
